e609f773d8ebb605df83adcb927dee04a6c9bb95
oussama <gridsoussama@gmail.com>
2023-08-07 13:53:16 +0100

    back

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7766;
 
 app.listen(PORT, () => console.log("server started on port: ", PORT));
