33b1d5c3e242eb6d8ec34572c07e91d482a45698
oussama <gridsoussama@gmail.com>
2023-08-07 22:31:34 +0100

    first commit

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 
-const PORT = process.env.PORT || 7766;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log("server started on port: ", PORT));
