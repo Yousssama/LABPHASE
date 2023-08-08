e18123fa5e26b805e82ffe01942509af3f1fa8af
oussama <gridsoussama@gmail.com>
2023-08-08 01:19:49 +0100

    uno

---
+++ client/build/precache-manifest.1881a1c70d861d576790db36e1822c4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0a8a4a376c082370a43cdbbec8621b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "128476364239b6ed256b",
+    "url": "/static/css/main.d646c616.chunk.css"
+  },
+  {
+    "revision": "77dad9edfe48803ffa9f",
+    "url": "/static/js/2.058421de.chunk.js"
+  },
+  {
+    "revision": "128476364239b6ed256b",
+    "url": "/static/js/main.9d9b92ca.chunk.js"
+  },
+  {
+    "revision": "c20bda0da91af47bb8f6",
+    "url": "/static/js/runtime-main.8fc0d6a4.js"
+  }
+]);
